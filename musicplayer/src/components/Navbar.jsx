f165aedf264f1d42d5ace73f1e2d03e4ee66f8f7
titoo9201 <titoos675@gmail.com>
2025-10-18 14:45:22 +0530

    nav compelete

--- musicplayer/src/components/Navbar.jsx
+++ musicplayer/src/components/Navbar.jsx
@@ -4,13 +4,15 @@ import { TiHome } from "react-icons/ti";
 import { BsFillSearchHeartFill } from "react-icons/bs";
 import { SlPlaylist } from "react-icons/sl";
 import { BsHearts } from "react-icons/bs";
+import {Link} from 'react-router-dom';
 function Navbar() {
   return (
-    <div className='w-full h-[100px] bg-black fixed top-0 text-white flex justify-center items-center gap-[50px]'>
-      <TiHome className='w-[60px] h-[60px]' />
-    <BsFillSearchHeartFill className='w-[60px] h-[60px]'/>
-    <SlPlaylist className='w-[60px] h-[60px]'/>
-    <BsHearts className='w-[60px] h-[60px]' />
+    <div className='w-full h-[100px] bg-black fixed  bottom-0  md:top-0 text-white flex justify-around md:justify-center items-center gap-[50px] p-[20px]'>
+    <Link to={"/"}><TiHome className='w-[28px] h-[28px]' /></Link>
+    <Link to={"search"}><BsFillSearchHeartFill className='w-[28px] h-[28px]'/></Link>
+    <Link to={"playlist"}><SlPlaylist className='w-[28px] h-[28px]'/></Link>
+    <Link to={"like"}><BsHearts className='w-[28px] h-[28px]' /></Link>
+  
     </div>
   )
 }
