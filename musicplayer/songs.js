2dde8aeb28c32cbf9a32d4d25ac932fd9d23497d
titoo9201 <titoos675@gmail.com>
2025-10-18 15:59:17 +0530

    song db in the form of json

---
+++ musicplayer/songs.js
@@ -0,0 +1,279 @@
+import song1 from "./assets/song1.mp3"
+import song2 from "./assets/song2.mp3"
+import song3 from "./assets/song3.mp3"
+import song4 from "./assets/song4.mp3"
+import song5 from "./assets/song5.mp3"
+import song6 from "./assets/song6.mp3"
+import song7 from "./assets/song7.mp3"
+import song8 from "./assets/song8.mp3"
+import song9 from "./assets/song9.mp3"
+import song10 from "./assets/song10.mp3"
+import song11 from "./assets/song11.mp3"
+import song12 from "./assets/song12.mp3"
+import song13 from "./assets/song13.mp3"
+import song14 from "./assets/song14.mp3"
+import song15 from "./assets/song15.mp3"
+import song16 from "./assets/song16.mp3"
+import song17 from "./assets/song17.mp3"
+import song18 from "./assets/song18.mp3"
+import song19 from "./assets/song19.mp3"
+import song20 from "./assets/song20.mp3"
+import song21 from "./assets/song21.mp3"
+import song22 from "./assets/song22.mp3"
+import song23 from "./assets/song23.mp3"
+import song24 from "./assets/song24.mp3"
+import song25 from "./assets/song25.mp3"
+
+import image1 from "./assets/image1.jpg"
+import image2 from "./assets/image2.jpg"
+import image3 from "./assets/image3.jpg"
+import image4 from "./assets/image4.jpg"
+import image5 from "./assets/image5.jpg"
+import image6 from "./assets/image6.jpg"
+import image7 from "./assets/image7.jpg"
+import image8 from "./assets/image8.jpg"
+import image9 from "./assets/image9.jpg"
+import image10 from "./assets/image10.jpg"
+import image11 from "./assets/image11.jpg"
+import image12 from "./assets/image12.jpg"
+import image13 from "./assets/image13.jpg"
+import image14 from "./assets/image14.jpg"
+import image15 from "./assets/image15.jpg"
+import image16 from "./assets/image16.jpg"
+import image17 from "./assets/image17.jpg"
+import image18 from "./assets/image18.jpg"
+import image19 from "./assets/image19.jpg"
+import image20 from "./assets/image20.jpg"
+import image21 from "./assets/image21.jpg"
+import image22 from "./assets/image22.jpg"
+import image23 from "./assets/image23.jpg"
+import image24 from "./assets/image24.jpg"
+import image25 from "./assets/image25.jpg"
+
+export const songsData = [
+  {
+    id: 1,
+    name: "Thekedara Ka Byah ",
+    song: song1,
+    image: image1,
+    category: "song",
+    liked: false,
+    singer: "Masoom Sharma "
+  },
+  {
+    id: 2,
+    name: "Sulfa",
+    song: song2,
+    image: image2,
+    category: "song",
+    liked: false,
+    singer: " Vikas Dhani Aala "
+  },
+  {
+    id: 3,
+    name: "Mittran Di Chhatri ",
+    song: song3,
+    image: image3,
+    category: "song",
+    liked: false,
+    singer: "Babu mann"
+  },
+  {
+    id: 4,
+    name: "College Mein",
+    song: song4,
+    image: image4,
+    category: "song",
+    liked: false,
+    singer: " Ajay Bhagta"
+  },
+  {
+    id: 5,
+    name: "Yamraaj ",
+    song: song5,
+    image: image5,
+    category: "song",
+    liked: false,
+    singer: "Gulzaar Chhaniwala"
+  },
+  {
+    id: 6,
+    name: "Supreme",
+    song: song6,
+    image: image6,
+    category: "song",
+    liked: false,
+    singer: "Shubh"
+  },
+  {
+    id: 7,
+    name: "Dhokha",
+    song: song7,
+    image: image7,
+    category: "song",
+    liked: false,
+    singer: "Masoom Sharma"
+  },
+  {
+    id: 8,
+    name: "Ramayan Ka Saar",
+    song: song8,
+    image: image8,
+    category: "song",
+    liked: false,
+    singer: "Masoom Sharma"
+  },
+  {
+    id: 9,
+    name: "Aangli Hawa Me ",
+    song: song9,
+    image: image9,
+    category: "song",
+    liked: false,
+    singer: "Manish Sonipat Aala "
+  },
+  {
+    id: 10,
+    name: "Ji Laage Se Babya Mai ",
+    song: song10,
+    image: image10,
+    category: "song",
+    liked: false,
+    singer: "Raj Mawar"
+  },
+  {
+    id: 11,
+    name: "For A Reason",
+    song: song11,
+    image: image11,
+    category: "song",
+    liked: false,
+    singer: " Karan Aujla"
+  },
+  {
+    id: 12,
+    name: "The Haryanvi Mashup 10",
+    song: song12,
+    image: image12,
+    category: "song",
+    liked: false,
+    singer: "group"
+  },
+  {
+    id: 13,
+    name: "The Haryanvi Mashup 6",
+    song: song13,
+    image: image13,
+    category: "song",
+    liked: false,
+    singer: "group"
+  },
+  {
+    id: 14,
+    name: "The Haryanvi Mashup 12",
+    song: song14,
+    image: image14,
+    category: "song",
+    liked: false,
+    singer: "group"
+  },
+  {
+    id: 15,
+    name: "Ghode 3",
+    song: song15,
+    image: image15,
+    category: "song",
+    liked: false,
+    singer: "Masoom Sharma "
+  },
+  {
+    id: 16,
+    name: "Pistol 4 5 Ka",
+    song: song16,
+    image: image16,
+    category: "song",
+    liked: false,
+    singer: " Masoom Sharma "
+  },
+  {
+    id: 17,
+    name: "STFU",
+    song: song17,
+    image: image17,
+    category: "song",
+    liked: false,
+    singer: "AP Dhillon"
+  },
+  {
+    id: 18,
+    name: "No Guts No Glory",
+    song: song18,
+    image: image18,
+    category: "song",
+    liked: false,
+    singer: " ADDY NAGAR "
+  },
+  {
+    id: 19,
+    name: "Udte Teer",
+    song: song19,
+    image: image19,
+    category: "song",
+    liked: false,
+    singer: "Masoom Sharma"
+  },
+  {
+    id: 20,
+    name: "Jija Ji",
+    song: song20,
+    image: image20,
+    category: "song",
+    liked: false,
+    singer: " Meeta Baroda"
+  },
+  {
+    id: 21,
+    name: "Dug Dug ",
+    song: song21,
+    image: image21,
+    category: "song",
+    liked: false,
+    singer: "Raj Mawar,"
+  },
+  {
+    id: 22,
+    name: "Chambal K Dakku",
+    song: song22,
+    image: image22,
+    category: "song",
+    liked: false,
+    singer: "Masoom Sharma"
+  },
+  {
+    id: 23,
+    name: "Blender",
+    song: song23,
+    image: image23,
+    category: "song",
+    liked: false,
+    singer: "Masoom Sharma"
+  },
+  {
+    id: 24,
+    name: "Russian Bandana ",
+    song: song24,
+    image: image24,
+    category: "song",
+    liked: false,
+    singer: "Dhanda Nyoliwala"
+  },
+  {
+    id: 25,
+    name: "SAB FADE JANGE ",
+    song: song25,
+    image: image25,
+    category: "song",
+    liked: false,
+    singer: "PARMISH VERMA "
+  },
+];
--- musicplayer/src/pages/Home.jsx
+++ musicplayer/src/pages/Home.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 function Home() {
   return (
     <div className='w-full h-[100vh] bg-black'>
-   
+     <div className='w-[50%] h-full bg-slate-700'><h1>Now Playing</h1></div>
+     <div></div>
     </div>
   )
 }
